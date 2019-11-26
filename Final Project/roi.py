e75d2ddc030f8e222e8355feae1132bb6e1f926a
Farrell Nathaniel <farrellnathaniel25@gmail.com>
2019-11-26 22:10:18 +0700

    finals

---
+++ Final Project/roi.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import cv2
+import numpy as np
+
+img = cv2.imread("Lenna.png")
+face = np.ones((200, 150, 3))
+cv2.imshow("Demo", img)
+face = img[200:400, 200:350]
+cv2.imshow("face", face)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
